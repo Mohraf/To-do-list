8950238ffce03997027e108dc007cfe8e282a2ba
mohraf <amosngok@gmail.com>
2021-12-16 16:22:02 +0300

    add todo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from './to-do';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/to-do/to-do.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../to-do';
+
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent implements OnInit {
+
+  todos: ToDo[] = [
+    new ToDo(1, 'Make Bed', ['Spread sheets', 'Fold blankets', 'Lay Pillows'], new Date(2021, 12, 17)),
+    new ToDo(2, 'Clean Room', ['Sweep floor', 'Mop floor'], new Date(2021, 12, 17)),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
