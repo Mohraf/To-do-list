def898fbf0c1073b93526487875dd47e6242d2af
mohraf <amosngok@gmail.com>
2021-12-16 16:25:12 +0300

    add view details button

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -13,6 +13,10 @@ export class ToDoComponent implements OnInit {
     new ToDo(2, 'Clean Room', ['Sweep floor', 'Mop floor'], new Date(2021, 12, 17)),
   ];
 
+  toggleDetails(index: number) {
+    this.todos[index].showDetails = !this.todos[index].showDetails;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
